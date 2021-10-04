9a27a896bb137a862c148f74e20cec4f7b9beecd
ivnmnndz <12gues@gmail.com>
2021-10-04 23:32:18 +0000

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const cardValue = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+const suites = [
+  {
+    name: "heart",
+    symbol: "♥️"
+  },
+  {
+    name: "diamond",
+    symbol: "♦"
+  },
+  {
+    name: "spade",
+    symbol: "♠"
+  },
+  {
+    name: "club",
+    symbol: "♣"
+  }
+];
+const getCard = () => {
+  const randomValue = cardValue[Math.floor(Math.random() * cardValue.length)];
+  const randomSuite = suites[Math.floor(Math.random() * suites.length)];
+  const cardDiv = document.getElementById("card");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  suites.forEach(suite => {
+    cardDiv.classList.remove(suite.name);
+  });
+  cardDiv.classList.add(randomSuite.name);
+
+  const cardEmojis = document.getElementsByClassName("cardEmoji");
+  const cardEmojiArray = [...cardEmojis];
+  cardEmojiArray.forEach(el => (el.innerHTML = randomSuite.symbol));
+  document.getElementById("cardInfo").innerHTML = `${randomValue}`;
 };
+
+getCard();
+document.querySelector("button").onclick = function() {
+  getCard();
+};
+
+window.onload = function() {};
